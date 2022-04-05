40a64c1001e6e7348ee8df2f764629fc9a4dbd18
Kunkka0822 <101172481+Kunkka0822@users.noreply.github.com>
2022-04-04 23:28:07 -0700

    wip

--- programs/x-token-staking/src/lib.rs
+++ programs/x-token-staking/src/lib.rs
@@ -19,4 +19,12 @@ pub mod x_token_staking {
     ) -> ProgramResult {
         create_vault::create_vault(ctx, reward_bump, reward_duration, stake_token_count)
     }
+
+    pub fn authorize_funder(ctx: Context<ControlFunder>, funder: Pubkey) -> ProgramResult {
+        control_funder::authorize_funder(ctx, funder)
+    }
+
+    pub fn unauthorize_funder(ctx: Context<ControlFunder>, funder: Pubkey) -> ProgramResult {
+        control_funder::unauthorize_funder(ctx, funder)
+    }
 }
